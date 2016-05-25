14305ead8b46ddeb5341b0bc26d4d4573ba42049
Antonin Jelinek <antonin.jelinek@gmail.com>
2016-05-25 14:16:33 +0200

    update

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ server.get(/.*/, restify.serveStatic({
     'default': 'index.html'
 }));
 
-server.listen(3978, function () {
+server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
